01e340a816577e03e6db69dee577f5731ec85606
pscacchi <pablo.scacchi@globalpay.com>
2023-02-19 02:05:53 -0300

    Text reacts to TextToSpeech

--- app/src/main/java/ar/com/westsoft/listening/MainViewModel.kt
+++ app/src/main/java/ar/com/westsoft/listening/MainViewModel.kt
@@ -1,15 +1,27 @@
 package ar.com.westsoft.listening
 
+import androidx.compose.ui.text.AnnotatedString
 import androidx.lifecycle.ViewModel
-import ar.com.westsoft.listening.data.Reader
+import androidx.lifecycle.viewModelScope
+import ar.com.westsoft.listening.data.SpeechProvider
+import ar.com.westsoft.listening.service.ReaderService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    val reader: Reader
+    val readerService: ReaderService,
+    val speechProvider: SpeechProvider
 ) : ViewModel() {
+
+    val annotatedStringStateFlow: StateFlow<AnnotatedString> =
+        readerService.getAnnotatedStringFlow()
+            .stateIn(viewModelScope, SharingStarted.Eagerly, AnnotatedString(""))
+
     fun onButtonClicked() {
-        reader.speakOut("Click on button!")
+        readerService.speakOut(speechProvider.getSpeech())
     }
 }
---
+++ app/src/main/java/ar/com/westsoft/listening/data/ReaderEngine.kt
@@ -0,0 +1,90 @@
+package ar.com.westsoft.listening.data
+
+import android.app.Application
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.speech.tts.Voice
+import android.util.Log
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.Locale
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.resume
+
+@Singleton
+class ReaderEngine @Inject constructor(
+    context: Application
+) {
+    private val tts: TextToSpeech = TextToSpeech(
+        context,
+    ) { status ->
+        if (status == TextToSpeech.ERROR) {
+            Log.i("SpeechToText", "Sintetizer init error")
+        } else if (status == TextToSpeech.SUCCESS) {
+            Log.i("SpeechToText", "Sintetizer init Success")
+        }
+    }
+
+    fun getUtteranceFlow() = flow {
+        while (true) {
+            emit(tts.awaitUtterance())
+        }
+    }
+    init {
+        tts.setPitch(1f)
+        tts.setSpeechRate(0.5f)
+        tts.setVoice(
+            Voice(
+                "en-us-x-sfg#male-local",
+                Locale.US,
+                400,
+                200,
+                true,
+                hashSetOf("male"),
+            ),
+        )
+    }
+
+    fun speakOut(message: String) {
+        tts.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+}
+
+data class Utterance(
+    val utteranceId: String? = null,
+    val start: Int = 0,
+    val end: Int = 0,
+    val frame: Int = 0,
+)
+
+suspend fun TextToSpeech.awaitUtterance() = suspendCancellableCoroutine<Utterance?> {
+    val listener = object : UtteranceProgressListener() {
+        override fun onStart(utteranceId: String?) {
+            println("*** onStart SpeakOut")
+        }
+
+        override fun onDone(utteranceId: String?) {
+            println("*** onDone SpeakOut")
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onError(utteranceId: String?) {
+            println("*** onError SpeakOut")
+        }
+
+        override fun onRangeStart(
+            utteranceId: String?,
+            start: Int,
+            end: Int,
+            frame: Int,
+        ) {
+            println("*** onRangeStart: utteranceId=$utteranceId, start=$start, end=$end, frame=$frame")
+            it.resume(Utterance(utteranceId, start, end, frame))
+        }
+    }
+    it.invokeOnCancellation {
+        setOnUtteranceProgressListener(null)
+    }
+    setOnUtteranceProgressListener(listener)
+}
