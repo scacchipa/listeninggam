efbe0072aa12e1c71084916f3929c9645b0fd5c0
PScacchi <pablo.scacchi@globalpay.com>
2023-09-09 15:52:35 -0300

    Add TODO list

--- app/src/main/java/ar/com/westsoft/listening/MainActivity.kt
+++ app/src/main/java/ar/com/westsoft/listening/MainActivity.kt
@@ -6,6 +6,13 @@ import androidx.activity.compose.setContent
 import ar.com.westsoft.listening.screen.menu.NavigationScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+// TODO: Delete speak out button
+// TODO: Add a config button in the DictGameScreen and to open a ConfigScreen
+// TODO: Add option to modified the count the game read
+// TODO: Add option to start to read before the cursor position.
+// TODO: Add option to download a txt file to the SelectDictationGameScreen
+// TODO: Add the option to paste a text from clipboard in the SelectDictationGameScreen
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
