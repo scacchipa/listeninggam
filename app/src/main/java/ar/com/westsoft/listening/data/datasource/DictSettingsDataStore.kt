524e6b7185a53186014c16bd153fbbb2698ab771
PScacchi <pablo.scacchi@globalpay.com>
2023-11-18 14:29:26 -0300

    Create DictSettingsDataStore test. Update Java version

--- app/src/main/java/ar/com/westsoft/listening/data/datasource/DictSettingsDataStore.kt
+++ app/src/main/java/ar/com/westsoft/listening/data/datasource/DictSettingsDataStore.kt
@@ -3,13 +3,9 @@ package ar.com.westsoft.listening.data.datasource
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -19,16 +15,6 @@ class DictSettingsDataStore @Inject constructor(
 ) {
     fun getDictGameSettingsDSOFlow(): Flow<DictGameSettingsDSO> =
         dataStore.data
-            .catch { exception ->
-                if (exception is IOException) {
-                    emit(emptyPreferences())
-                } else {
-                    throw exception
-                }
-            }
-            .onEach {
-                println("Preferences changed")
-            }
             .map { preferences ->
                 preferences.getDictGameSettingsDSO()
             }
@@ -38,9 +24,8 @@ class DictSettingsDataStore @Inject constructor(
             ?: preferenceField.defaultValue
     }
 
-    suspend inline fun <reified T> save(preferenceField: PreferencesKey<T>, value: T?) {
+    suspend inline fun <reified T> save(preferenceField: PreferencesKey<T>, value: T?) =
         dataStore.edit { preferences ->
             preferences[preferenceField.key] = value ?: preferenceField.defaultValue
         }
-    }
 }
