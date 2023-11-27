849e2ec71e7408cc3117f6d726f4c802b4139abf
PScacchi <pablo.scacchi@globalpay.com>
2023-11-26 22:07:17 -0300

    Create SavedDictationGameDao test.

---
+++ app/src/androidTest/java/ar/com/westsoft/listening/data/datasource/SavedDictationGameDaoTest.kt
@@ -0,0 +1,162 @@
+package ar.com.westsoft.listening.data.datasource
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import ar.com.westsoft.listening.data.game.DictationGameHeader
+import ar.com.westsoft.listening.data.game.DictationGameRecord
+import ar.com.westsoft.listening.data.game.DictationProgress
+import ar.com.westsoft.listening.mapper.DictationProgressListMapper
+import ar.com.westsoft.listening.mapper.DictationProgressMapper
+import ar.com.westsoft.listening.mapper.GameHeaderMapper
+import ar.com.westsoft.listening.mapper.SavedDictationGameMapper
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+class SavedDictationGameDaoTest {
+
+    private val gameHeaderEntity1 = GameHeaderEntity(234L, "aTitle1", "address1", 12.3)
+    private val gameHeaderEntity2 = GameHeaderEntity(111L, "aTitle2", "address2", 11.0)
+    private val gameHeaderEntity3 = GameHeaderEntity(234L, "aTitle3", "address3", 12.0)
+
+    private val gameHeader = DictationGameHeader(0, "New title", "address", 10.3)
+    private val progressList = listOf(
+        DictationProgress(null, "Text one.", "____ ___.".toCharArray()),
+        DictationProgress(null, "Text two.", "____ ___.".toCharArray())
+    )
+
+    private lateinit var db: AppDatabase
+    private lateinit var gameDao: SavedDictationGameDao
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        gameDao = db.getSavedListeningGameDao()
+
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun insertHeader() {
+        gameDao.insertHeader(gameHeaderEntity1)
+        gameDao.insertHeader(gameHeaderEntity2)
+
+        val actual = gameDao.getSavedDictationGameEntityList().map { it.gameHeaderEntity }
+        val expected = listOf(gameHeaderEntity2, gameHeaderEntity1)
+
+        assert(actual == expected)
+    }
+
+    @Test
+    fun updateHeader() {
+        gameDao.insertHeader(gameHeaderEntity1)
+        gameDao.updateHeader(gameHeaderEntity3)
+
+        val actual = gameDao.getSavedDictationGameEntityList().map { it.gameHeaderEntity }
+        val expected = listOf(gameHeaderEntity3)
+
+        assert(actual == expected)
+    }
+
+    @Test
+    fun deleteHeader() {
+        gameDao.insertHeader(gameHeaderEntity1)
+        gameDao.insertHeader(gameHeaderEntity2)
+        gameDao.deleteWholeGame(gameHeaderEntity1)
+
+        val actual = gameDao.getSavedDictationGameEntityList().map { it.gameHeaderEntity }
+        val expected = listOf(gameHeaderEntity2)
+
+        assert(actual == expected)
+    }
+
+    @Test
+    fun insertGameEntity() {
+        val gui = gameDao.insertGameEntity(
+            game = SavedDictationGameMapper(
+                dictationProgressListMapper = DictationProgressListMapper(
+                    dictationProgressMapper = DictationProgressMapper()
+                ),
+                gameHeaderMapper = GameHeaderMapper()
+            ).toDataSource(
+                origin = DictationGameRecord(
+                    gameHeader = gameHeader,
+                    dictationProgressList = progressList
+                )
+            )
+        )
+
+        val actual = gameDao.getSavedDictationGameEntityList()
+        val expected = listOf(
+            SavedDictationGameEntity(
+                GameHeaderEntity(gui, "New title", "address", 10.3),
+                listOf(
+                    DictationProgressEntity(1, gui, "Text one.", "____ ___."),
+                    DictationProgressEntity(2, gui, "Text two.", "____ ___.")
+                )
+            )
+        )
+
+        assert(actual == expected)
+    }
+
+
+    @Test
+    fun insertGameHeaderAndProgress() {
+        val actualGui = gameDao.insertGameHeaderAndProgress(
+            gameHeaderEntity = gameHeaderEntity1,
+            progressEntityList = DictationProgressListMapper(
+                dictationProgressMapper = DictationProgressMapper()
+            ).toDataSource(
+                origin = progressList
+            )
+        )
+        val expectedGui = 234L
+        val actual = gameDao.getSavedDictationGameEntityList()
+        val expected = listOf(
+            SavedDictationGameEntity(
+                gameHeaderEntity = gameHeaderEntity1,
+                dictationProgressEntityLists =
+                listOf(
+                    DictationProgressEntity(1, gameHeaderEntity1.gui, "Text one.", "____ ___."),
+                    DictationProgressEntity(2, gameHeaderEntity1.gui, "Text two.", "____ ___.")
+                )
+            )
+        )
+
+        assert(actualGui == expectedGui)
+        assert(actual == expected)
+    }
+
+    @Test
+    fun deleteWhole() {
+        gameDao.insertGameEntity(
+            game = SavedDictationGameMapper(
+                dictationProgressListMapper = DictationProgressListMapper(
+                    dictationProgressMapper = DictationProgressMapper()
+                ),
+                gameHeaderMapper = GameHeaderMapper()
+            ).toDataSource(
+                origin = DictationGameRecord(
+                    gameHeader = gameHeader,
+                    dictationProgressList = progressList
+                )
+            )
+        )
+
+        gameDao.deleteWholeGame(
+            GameHeaderEntity(234, "New title", "address", 10.3)
+        )
+
+        assert(gameDao.getSavedDictationGameEntityList().isEmpty())
+    }
+}
