d75d0cb95c5e263fb8b8606217dab7712d04cbc8
ScacchiPA <scacchipa@gmail.com>
2025-08-25 01:25:55 -0300

    Update Hilt Version

--- epub/build.gradle.kts
+++ epub/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.devtools.ksp)
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.compose)
 }
 
